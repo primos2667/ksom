0f58bc12b04db06ba47bd6792d1e88d161477412
Primos <user@Primos-Macbook-Air.local>
2026-08-29 15:16:28 +0000

    clean supabase files - remove conflict

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,7 @@
 import { createBrowserClient } from "@supabase/ssr";
-
 export function createClient() {
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
   const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
   return createBrowserClient(url, key);
 }
-
 export const supabase = createClient();
