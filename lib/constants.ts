dabc910793da66b2271a867271c3a473239ec42e
Primos <user@Primos-Macbook-Air.local>
2026-08-29 05:51:13 +0000

    add constants file

---
+++ lib/constants.ts
@@ -0,0 +1,21 @@
+export const CATEGORIES = [
+  "All",
+  "Electronics",
+  "Fashion",
+  "Books",
+  "Home & Hostel",
+  "Food",
+  "Services",
+  "Beauty",
+  "Gadgets",
+  "Other"
+];
+
+export const KNUST_LOCATIONS = [
+  "Ayeduase",
+  "Kotei",
+  "Boadi",
+  "Campus",
+  "Tech Junction",
+  "Kentinkrono"
+];
